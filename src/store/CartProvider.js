94a998cfac6b0b45c59baf504154f66654a919d0
Harish1651999 <hk1651999@gmail.com>
2022-12-07 21:21:15 +0530

    add refs and forward refs.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
 const defaultCartState = {
@@ -10,7 +10,7 @@ const cartReducer = (state, action) => {
   if (action.type === "ADD") {
     const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
-      state.totalAmount + state.item.price * state.item.amount;
+      state.totalAmount + action.item.price * action.item.amount;
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
@@ -33,14 +33,13 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
-  const cartContext = [
-    {
-      items: cartState.items,
-      totalAmount: cartState.totalAmount,
-      addItem: addItemToCartHandler,
-      removeItem: removeItemFromCartHandler,
-    },
-  ];
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
   return (
     <CartContext.Provider value={cartContext}>
       {props.children}
